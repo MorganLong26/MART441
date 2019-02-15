393249c2758ce8e5b4feb13786f04148411b54f4
Morgan Long <mraelong26@gmail.com>
2019-02-14 23:48:06 -0700

    finished assignment 5

---
+++ hw-5/Readme.md
@@ -0,0 +1,5 @@
+# Assignment 5
+This week we had to create the beginning of the memory card game. I wanted to go with the theme of sharks. I chose five different pictures of sharks and for the back of the cards I chose your stereo typical backing for a deck of cards. This week we also implemented arrays and cookies. I have had experience with arrays in Creative Coding 1, but I have never dealt with cookies before.
+
+## Issues
+This week I mostly followed all of the video tutorials on the course website and had a few minor issues. I was able to get all 10 cards to show up and have the ability to flip and show a picture underneath. The issue I ran into was that only two of my five images were popping up under all 10 cards. When I emailed our professor about my issue I found out that it was because my var count for my images was set to only two zeros instead of five, and my count random number was set to five instead of two.  
---
+++ hw-5/script/main.js
@@ -0,0 +1,44 @@
+
+var imageTags = ["image1", "image2", "image3", "image4","image5","image6","image7","image8","image9","image10"];
+var blankImagePath = "images/card.jpg";
+var actualImages = new Array();
+
+function printBlanks()
+{
+    createRandomImageArray();
+    for(var i = 0; i < imageTags.length; i++)
+    {
+
+        document.getElementById(imageTags[i]).src= blankImagePath;
+    }
+
+}
+
+function createRandomImageArray()
+{
+
+    var actualImagePath = ["Images/BullShark.jpg", "Images/GreatWhite.jpg", "Images/ThresherShark.jpg", "Images/GoblinShark.jpg", "Images/Tiger_shark.jpg"];
+    var count = [0,0,0,0,0];
+
+    while(actualImages.length < 10)
+    {
+
+        var randomNumber = Math.floor(Math.random() * actualImagePath.length)
+
+        if(count[randomNumber] < 2)
+        {
+            actualImages.push(actualImagePath[randomNumber]);
+            count[randomNumber] = count[randomNumber] + 1;
+        }
+    }
+
+
+}
+
+function flipImage(number)
+{
+    document.getElementById(imageTags[number]).src= actualImages[number];
+
+
+
+}
