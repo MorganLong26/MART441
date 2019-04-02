d5cbff3fb9c4be53c106b677da71ce8a7931fada
Morgan Long <mraelong26@gmail.com>
2019-04-01 21:11:49 -0600

    Fixed some issues

--- hw_9/scripts/jsonajax.js
+++ hw_9/scripts/jsonajax.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
            $("button").click(function () {
-               $("#walkingInformation").load("data/walkiing.json", function(responseText){
+               $("#walkingInformation").load("data/walking.json", function(responseText){
                    var walking = JSON.parse(responseText);
                    $("#walkingInformation").html("Title: " + walking.title
                    + "<br>Type:" + walking.type + "<br>Language:" + walking.language +"<br>Genres:<br>" +
